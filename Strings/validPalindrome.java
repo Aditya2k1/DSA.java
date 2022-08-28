33878713bfb3b145d21ae1b6bdb6f71f561a3709
ADITYA SHAW <adityashaw078@yahoo.com>
2022-08-28 13:40:57 +0530

    Update validPalindrome.java

--- Strings/validPalindrome.java
+++ Strings/validPalindrome.java
@@ -37,3 +37,38 @@ public class validPalindrome{
         System.out.println(validPalindrome(s));
     }
 }
+
+------------------------------------------------------------------------------or---------------------------------------------------------------------------------------
+
+//Runtime - 0ms
+//Source  - https://programs.programmingoneonone.com/2021/08/leetcode-valid-palindrome-problem-solution.html
+
+int i =0, j = s.length()-1;
+    while(i<j) {
+        while(i<s.length() && !isAlphaNumeric(s.charAt(i))) i++;
+        while(j>=0 && !isAlphaNumeric(s.charAt(j))) j--;
+        if (j>i && !compare(s.charAt(i), s.charAt(j))) {
+            return false;
+        }
+        i++;j--;
+    }
+        
+   return true;           
+}
+private boolean compare(char a, char b) {
+    if (Character.isUpperCase(a)) {
+        a = (char)(a + 32);
+    }
+    if (Character.isUpperCase(b)) {
+        b = (char)(b +32);
+    }
+    return a == b;
+}
+
+private boolean isAlphaNumeric(char c) {
+    if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' ||
+        c>= '0' && c <='9') {
+        return true;
+    }
+    return false;
+}
